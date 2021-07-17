9519473633828617d4bf53abb9b5402aa896ceac
tisumaki <84583619+tisumaki@users.noreply.github.com>
2021-07-17 12:40:42 +0300

    First draft of the finished project

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction () {
+    var mybutton = document.getElementById("myBtn");
+    if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
